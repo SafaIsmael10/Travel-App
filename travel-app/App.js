dc8ebd73c8fa2b5922cd919d02e154acdba83155
SafaIsmael <k2060345@kingston.ac.uk>
2024-12-14 15:47:34 +0000

    App Now Working

--- travel-app/App.js
+++ travel-app/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import HomeScreen from "./src/components/screens/HomeScreen.js";
+import HomeScreen from "./app/HomeScreen.js";
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
---
+++ travel-app/app/HomeScreen.js
@@ -0,0 +1,18 @@
+import Screen from "./Screen.js";
+import { View, Text } from "react-native";
+
+const HomeScreen = () =>
+{
+    return (
+        <Screen>
+            <Text>Home Screen !!!</Text>
+            <Text>Home Screen !!!</Text>
+            <Text>Home Screen !!!</Text>
+            <Text>Home Screen !!!</Text>
+            <Text>whats good hamodi</Text>
+            <Text>SIGMA SIGMA BOY</Text>
+        </Screen>
+      );
+};
+
+export default HomeScreen;
